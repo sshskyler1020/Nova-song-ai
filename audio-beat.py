5a901ad2078a8faf47f456a12fa33c41de59e08b
sshskyler1020 <sshskyler@gmail.com>
2025-12-19 09:35:16 -0500

    Delete audio-beat.py

--- audio-beat.py
+++
@@ -1,9 +0,0 @@
-import librosa
-from pydub import AudioSegment
-
-def snap_beat(beat_audio, vocals, strength=0.8):
-    # Simple placeholder snapping for demo
-    final = beat_audio
-    for v in vocals:
-        final = final.overlay(v)
-    return final
